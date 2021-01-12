e25dca44fb4b7554280efcfdf9a00e03960b8c3e
Stefan <baasvancode@gmail.com>
2021-01-12 08:17:31 +0000

    k7s build and deployment

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t baasvancodeza/multi-client:latest -t baasvancodeza/multi-client:$SHA ./client
+docker build -t baasvancodeza/multi-server:latest -t baasvancodeza/multi-server:$SHA ./server
+docker build -t baasvancodeza/multi-worker:latest -t baasvancodeza/multi-worker:$SHA ./worker
+# Push to dockerhub
+docker push baasvancodeza/multi-client:latest
+docker push baasvancodeza/multi-client:$SHA
+docker push baasvancodeza/multi-server:latest
+docker push baasvancodeza/multi-server:$SHA
+docker push baasvancodeza/multi-worker:latest
+docker push baasvancodeza/multi-worker:$SHA
+# deploy k8s
+kubectl -f k8s
+kubectl set image deployments/server-deployment server=baasvancodeza/multi-server:$SHA
+kubectl set image deployments/client-deployment client=baasvancodeza/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=baasvancodeza/multi-wroker:$SHA
